53ae8bbbc1f36b368959d5b4fc10f3d15bac92b7
baocxa <bao.nguyen@cxagroup.com>
2020-07-28 16:44:10 +0700

    add package

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   platform: "github",
-  token: "fdb54fc345576fd18641ca7cc7b8d388d1022fe5",
+  token: "fd5f666761005aabc38c25b29e77bc262d1de59e",
   logFileLevel: "warn",
   logLevel: "debug",
   onboarding: false,
